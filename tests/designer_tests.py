9605a66fde0a2b553b35c7313a31c95dec572679
drhaley <14882746+drhaley@users.noreply.github.com>
2019-09-03 19:10:42 -0700

    Travis integration (#2)
    * Create .travis.yml
    
    * Download Nupack
    
    * bugfix in bash string concatentation
    
    * wget fix
    
    * another wget fix
    
    * bugfix 401 error
    
    * build, install nupack
    
    * Run all oracle tests

--- tests/designer_tests.py
+++ tests/designer_tests.py
@@ -1,5 +1,8 @@
 import itertools
 import unittest
+import os
+import abc
+import importlib
 
 #add the project dir to the python path
 import sys
@@ -11,46 +14,58 @@ sys.path.append(
 )
 
 
+
 ###############################################
-#TODO: Iterate over all oracles
-import oracle.nupack as oracle_lib
-#import oracle.debug as oracle_lib
-###############################################
-#choose which sequence iterator to use here.  TODO: tie this option to the command-line
+#choose which sequence iterator to use here.
 import sequence_iterator.exhaustive as sequence_iterator_lib
 ###############################################
 
-class oracle_tests(unittest.TestCase):
+class OracleTests(unittest.TestCase):
 	__TEMPERATURE = 40.0
 
 	def __init__(self, *args):
-		self.__oracle = oracle_lib.Oracle()
+		self.__oracle_list = [
+			importlib.import_module(f"oracle.{oracle_name}").Oracle()
+			for oracle_name in self.__oracle_names()
+		]
 		super().__init__(*args)
 
+	def __oracle_names(self):
+		oracle_names = []
+		for file in os.listdir("oracle"):
+			if file.endswith(".py"):
+				oracle_name = os.path.splitext(file)[0]
+				if oracle_name.lower() != "abstract":
+					oracle_names.append(oracle_name)
+		return oracle_names
+
 	def test_hairpin(self):
-		strong_binding_energy = self.__oracle.self_affinity(
-			"GGGGGGGGGGGGGGGGGGGGGGGGGAAATCCCCCCCCCCCCC",
-			self.__TEMPERATURE
-		)
-		self.assertTrue(strong_binding_energy < 0.0)
-		weak_binding_energy = self.__oracle.self_affinity(
-			"GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG",
-			self.__TEMPERATURE
-		)
-		self.assertTrue(weak_binding_energy >= 0.0)
+		for oracle in self.__oracle_list:
+			with self.subTest(oracle = oracle):
+				strong_binding_energy = oracle.self_affinity(
+					"GGGGGGGGGGGGGGGGGGGGGGGGGAAATCCCCCCCCCCCCC",
+					self.__TEMPERATURE
+				)
+				self.assertTrue(strong_binding_energy < 0.0)
+				weak_binding_energy = oracle.self_affinity(
+					"GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG",
+					self.__TEMPERATURE
+				)
+				self.assertTrue(weak_binding_energy >= 0.0)
 
 	def test_binding(self):
-		seq1 = "CCCC"
-		seq2 = "GGGG"
+		for oracle in self.__oracle_list:
+			with self.subTest(oracle = oracle):
+				seq1 = "CCCC"
+				seq2 = "GGGG"
 
-		strong_binding_energy = self.__oracle.binding_affinity(seq1, seq2, self.__TEMPERATURE)
-		self.assertTrue(strong_binding_energy < 0.0)
+				strong_binding_energy = oracle.binding_affinity(seq1, seq2, self.__TEMPERATURE)
+				self.assertTrue(strong_binding_energy < 0.0)
 
-		weak_binding_energy = self.__oracle.binding_affinity(seq1, seq1, self.__TEMPERATURE)
-		self.assertTrue(weak_binding_energy > 0.0)
+				weak_binding_energy = oracle.binding_affinity(seq1, seq1, self.__TEMPERATURE)
+				self.assertTrue(weak_binding_energy >= 0.0)
 
-
-class sequence_iterator_checks(unittest.TestCase):
+class SequenceIteratorChecks(unittest.TestCase):
 	def __init__(self, *args):
 		self.__sequence_iterator = sequence_iterator_lib.SequenceIterator()
 		super().__init__(*args)
@@ -61,7 +76,7 @@ class sequence_iterator_checks(unittest.TestCase):
 			seq = self.__sequence_iterator.next()
 	
 	
-class python_syntax_checks(unittest.TestCase):
+class PythonSyntaxChecks(unittest.TestCase):
 	def test_f_strings(self):
 		self.assertEqual(f"{3}", "3")
 		self.assertNotEqual(f"{4}", "3")
